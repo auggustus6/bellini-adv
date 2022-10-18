9f7c8f34f0684225d09a8cb339e5c49f8df0cb45
guusfelix2015 <gusfelix2015@gmail.com>
2022-10-17 21:40:59 -0300

    Fix: build

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -39,7 +39,6 @@ export const Social = styled.div`
 export const ContainerCopy = styled.div`
   margin: 0 auto;
   width: 100%;
-
   display: flex;
   align-items: center;
   justify-content: center;
--- src/components/MenuFooter/styles.ts
+++ src/components/MenuFooter/styles.ts
@@ -21,8 +21,7 @@ export const NavFooter = styled.div`
 
     ul li a {
       color: ${theme.colors.darkBlack};
-      font-weight: bold;
-      
+      font-weight: 700;
     }
   `}
 `;
